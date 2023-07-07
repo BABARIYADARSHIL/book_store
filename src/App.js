cae727bb8e9b6561e6474e811504f9f8e245f24e
BABARIYADARSHIL <darshilbabariya444@gmail.com>
2023-07-07 13:45:21 +0530

    completed project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+//import redux from "@reduxjs/toolkit" 
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { loadIcon } from "./assets";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-
 import Searchbar from "./Components/Searchbar";
 import MyNavigation from "./MyNavigation";
 import store from "./State/store";
